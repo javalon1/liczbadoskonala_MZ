bb1371e5df885d114eaf982ea942d37a23818f3c
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-18 10:32:48 +0100

    Zmiany, zmiany

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,33 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+    Scanner sk = new Scanner(System.in);
+    int x = sk.nextInt();
+
+    int sumaDzielenia = 0;
+    int i = 1;
+
+        while (i <= x/2) {
+
+            if (x % i == 0) {
+                sumaDzielenia = sumaDzielenia + i;
+            }
+            i = i + 1;
+
+        }
+        if (x == sumaDzielenia) {
+            System.out.println(x + " " + "Jest liczba doskonala.");
+
+        } else {
+            System.out.println(x + " " + "Nie jest liczba doskonala.");
+        }
     }
+
 }
